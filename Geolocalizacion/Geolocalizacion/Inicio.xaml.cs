c20c33bf6dbe86ca766c1d1688e1246d7030a192
Jose Ignacio Rodríguez García <JoseIgnacio.Rodriguez@tajamar365.com>
2016-02-02 12:23:46 +0100

    no message

---
+++ Geolocalizacion/Geolocalizacion/Inicio.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+using Xamarin.Forms;
+
+namespace Geolocalizacion
+{
+    public partial class Inicio : ContentPage
+    {
+
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var loc = CrossGeolocator.Current; //referencia actual del geolocalizador para la plataforma
+            var pos = await loc.GetPositionAsync(10000);
+            if (pos == null)
+            {
+                await DisplayAlert("Error", "No recibo la posicion", "ok");
+                return;
+            }
+
+
+            TxtLat.Text = pos.Latitude.ToString();
+            TxtLon.Text = pos.Longitude.ToString();
+        }
+    }
+}
